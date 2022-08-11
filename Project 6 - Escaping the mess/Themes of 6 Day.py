0d497a91e1b784940f5e3e4c62c25356d8a644dd
AntonKolomytsev <info@world-geo.ru>
2022-08-11 14:30:07 +0300

    commit

---
+++ Project 6 - Escaping the mess/Themes of 6 Day.py
@@ -0,0 +1,34 @@
+## Themes of 6 day
+
+# Theme1 Functions
+## https://docs.python.org/3/library/functions.html - Library functions of Python
+## with keyword DEF we can write our own function, after def we must add name of our function and add : in the end
+##  Sintaxis
+## def my_function():
+##      Do this
+##      Then do this
+##      Finally do this
+
+def my_function():
+    print("Hello")
+    print("Bye")
+## After we use calling this function
+my_function()
+
+
+#Theme2  Indentation
+# There means that each command after def must be move from start string
+# We can use attachment style equals 1 space for identation
+
+
+
+#3 Theme 3 While Loop
+# while something_is_True:
+#     # Do something repetedly
+steps = 6
+while steps >0:
+    steps -=1
+    print(f"Before finish :  {steps}" )
+    # Infinite Loop - the loop with wrong value for example
+    # while 5>3:
+    #Do something
